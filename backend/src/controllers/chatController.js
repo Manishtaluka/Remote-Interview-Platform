90e453e5c63b263d74e153e9d34e855753dd3a00
Manish Taluka <manishtaluka413@gmail.com>
2026-06-11 15:16:34 +0530

    dashboard page added

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,17 +1,18 @@
-export async function getstreamToken(req,res){
-    try{
-        //use clerkId for stream,(not mongodb id ) it should match the id in our dashboard of stream
-        const token=chatClient.createToken(req.user.clerkId);
-        res.status(200).json({
-            token,
-            userId:req.user.clerkId,
-            userImage:req.user.image,
-            userName:req.user.name,
+import { chatClient } from "../lib/stream.js";
 
-        });
+export async function getStreamToken(req, res) {
+  try {
+    // use clerkId for Stream (not mongodb _id)=> it should match the id we have in the stream dashboard
+    const token = chatClient.createToken(req.user.clerkId);
 
-    }catch(error){
-        console.log("Error in getstreamToken controller:",error.message);
-        res.status(500).json({msg:"Internal Server Error"});
-    }
+    res.status(200).json({
+      token,
+      userId: req.user.clerkId,
+      userName: req.user.name,
+      userImage: req.user.image,
+    });
+  } catch (error) {
+    console.log("Error in getStreamToken controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
